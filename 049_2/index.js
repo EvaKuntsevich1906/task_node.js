4be6ad7f6189997a5bcf4e8a5c183eea912e83c2
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-04-23 00:26:09 +0300

    task

--- 049_2/index.js
+++ 049_2/index.js
@@ -1,22 +1,50 @@
 const express = require("express");
-const {getByIddate} = require("./service")
+const bodyParser = require("body-parser");
+
+const {
+    getByIddate,
+    createDate,
+    updateDateByiD,
+    deleteData
+} = require("./service")
+
 
 const app = express();
 
-const port = 8000;
+app.use(bodyParser.json())
+const port = 7000;
 
-app.get("/", function(req, res) {
-res.send()
+app.get("/", function (req, res) {
+    res.send("GET")
 });
 
-app.post("/", function(req, res) {
-    res.send()
+
+app.post("/", function (req, res) {
+    const {
+        name,
+        age
+    } = req.body;
+    const send = createDate(name, age);
+    res.send(send)
 });
-app.put("/", function(req, res) {
-    res.send()
+
+
+app.put("/:id", function (req, res) {
+    const {
+        id
+    } = req.params;
+    const send = updateDateByiD(id)
+    res.send(send)
+
 });
-app.delete("/", function(req, res) {
-    res.send()
+
+
+app.delete("/:id", function (req, res) {
+    const {
+        id
+    } = req.params;
+    const send = deleteData(id)
+    res.send(send)
 });
 
 app.listen(port, function () {
--- 049_2/service.js
+++ 049_2/service.js
@@ -25,12 +25,45 @@ const arr = [{
     }
 ];
 
+function createDate(name, age) {
+    const obj = {
+        id: arr.length + 1,
+        name: name,
+        age: age
+    }
+    arr.push(obj)
+    return arr
+};
 
 function getByIddate(id) {
     const filtId = arr.filter(el => el.id = id);
     return filtId
 }
 
+
+function updateDateByiD(id, name, age) {
+    const obj = {
+        id: id,
+        name: name,
+        age: age
+    }
+    const filterId = arr.filter(el => el.id != id);
+    if (arr.length != filterId.length) {
+        filterId.push(obj)
+        return filterId
+    } else {
+        return "Not Found"
+    }
+}
+
+function deleteData(id) {
+    const filterId = arr.filter(el => el.id != id);
+    return filterId
+}
+
 module.exports = {
-    getByIddate
+    getByIddate,
+    createDate,
+    updateDateByiD,
+    deleteData
 }
